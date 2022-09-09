6ff37d58f069f2bb0a3168b549044f532706d89e
Shubhamklanje <shubhamklanje9421@gmail.com>
2022-09-09 17:51:17 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React,{useEffect} from 'react';
 import './App.css';
+import Login from './components/Login';
+import {useStateProvider} from './assests/StateProvider';
+import { reducerCases } from './assests/Constants';
+import Spotify from './components/Spotify';
+
 
 function App() {
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  },[dispatch, token]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        token ? <Spotify/> : <Login/>
+      }
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="Slogoo"><h3>Made by SHUBHAM K LANJE :)</h3> </div>
+  )
+}
